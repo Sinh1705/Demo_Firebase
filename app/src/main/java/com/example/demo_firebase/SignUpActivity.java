cb03b9d01842afb9f2c241257ccb92f58eb0011f
Sinh1705 <sinhteogp@gmail.com>
2024-02-28 10:29:33 +0700

    commit 28/2/2024

--- app/src/main/java/com/example/demo_firebase/SignUpActivity.java
+++ app/src/main/java/com/example/demo_firebase/SignUpActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class SignUpActivity extends AppCompatActivity {
     
@@ -48,20 +49,54 @@ public class SignUpActivity extends AppCompatActivity {
         String stremail = edtEmail.getText().toString().trim();
         String strpass = edtPassword.getText().toString().trim();
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.createUserWithEmailAndPassword(stremail, strpass)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+        // Kiểm tra xem địa chỉ email đã được đăng ký hay chưa
+        auth.fetchSignInMethodsForEmail(stremail)
+                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
-                            startActivity(intent);
-                            finishAffinity();
+                            SignInMethodQueryResult result = task.getResult();
+                            if (result != null && result.getSignInMethods() != null && !result.getSignInMethods().isEmpty()) {
+                                // Email đã tồn tại, hiển thị thông báo hoặc thực hiện hành động phù hợp
+                                Toast.makeText(SignUpActivity.this, "Email đã tồn tại", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // Email chưa được đăng ký, tiếp tục quá trình đăng ký
+                                auth.createUserWithEmailAndPassword(stremail, strpass)
+                                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (task.isSuccessful()) {
+                                                    // Quá trình đăng ký thành công
+                                                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                                                    UpdateUI(firebaseUser);
+                                                } else {
+                                                    // Quá trình đăng ký thất bại
+                                                    Toast.makeText(SignUpActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                                    UpdateUI(null);
+                                                }
+                                            }
+                                        });
+                            }
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(SignUpActivity.this,"Authentication Failed",Toast.LENGTH_SHORT).show();
+                            // Xử lý lỗi nếu có
+                            Exception exception = task.getException();
+                            Toast.makeText(SignUpActivity.this, "Lỗi: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+
+
+    private void UpdateUI(FirebaseUser user ){
+
+        if(user != null){
+            Intent intent = new Intent(SignUpActivity.this,SigninActivity.class);
+            startActivity(intent);
+            finish();
+        }else {
+            Toast.makeText(SignUpActivity.this,"Chưa nhập thông tin đăng ký",Toast.LENGTH_SHORT).show();
+        }
     }
 }
